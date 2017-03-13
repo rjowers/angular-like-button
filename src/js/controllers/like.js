c7f0ab6a49f61687fa2bcfba4ff6de41d20d9c00
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-13 15:00:52 -0400

    wired up like controller

---
+++ src/js/controllers/like.js
@@ -0,0 +1,8 @@
+function LikeController ($scope) {
+  console.log("hello");
+   console.log($scope);
+}
+
+
+
+export default LikeController;
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import angular from 'angular';
+
+import LikeController from './controllers/like';
+
+angular
+  .module('likeular', [])
+  .controller('LikeController', LikeController)
