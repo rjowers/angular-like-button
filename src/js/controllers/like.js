cf8c6526cebb5383ba90724f459d701d04c9db26
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-13 20:47:08 -0400

    made Likes turn to singular when the count is 1

--- src/js/controllers/like.js
+++ src/js/controllers/like.js
@@ -1,7 +1,18 @@
-function LikeController ($scope) {
-  console.log("hello");
-   console.log($scope);
+function LikeController ($scope){
+  $scope.count=0;
+  $scope.Likes='Likes';
+  $scope.Like = function() {
+    $scope.count++;
+    if ($scope.count === 1) {
+      $scope.Likes = 'Like'
+
+    }
+  }
+  $scope.Unlike=function (){
+    $scope.count--;
+  }
 }
+
 
 
 
